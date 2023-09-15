30b6f0e89786c0a9577cbb082dd945649b3f3a5f
echen12 <chen.edmond13@gmail.com>
2023-09-15 15:53:34 -0700

    moved hardcoded strings to constants

--- COMP4537/labs/1/js/reader.js
+++ COMP4537/labs/1/js/reader.js
@@ -1,47 +1,54 @@
+const CARD_CONTAINER_ID = 'cardContainer';
+const CARD_TEXT_CONTENT_ID = 'cardTextContent';
+const UPDATED_TEXT_ID = 'updatedText';
+const UPDATED_AT_TEXT = 'updated at: ';
+const STORAGE_KEY = 'testArray';
+const CARD_CLASS_NAME = 'card';
+const CARD_BODY_CLASS_NAME = 'card-body';
+const CARD_TEXT_CLASS_NAME = 'card-text';
+const CARD_WIDTH = '18rem';
+const INTERVAL_DELAY_MS = 2000;
+
 function createCard(key, value) {
-    
     let card = document.createElement('div');
     card.id = key;
-    card.className = 'card';
-    card.style.width = '18rem';
+    card.className = CARD_CLASS_NAME;
+    card.style.width = CARD_WIDTH;
 
     // Create card body element
     let cardBody = document.createElement('div');
-    cardBody.className = 'card-body';
+    cardBody.className = CARD_BODY_CLASS_NAME;
 
     // Create card text element
     let cardText = document.createElement('p');
-    cardText.id = 'cardTextContent';
-    cardText.className = 'card-text';
+    cardText.id = CARD_TEXT_CONTENT_ID;
+    cardText.className = CARD_TEXT_CLASS_NAME;
     cardText.textContent = value;
 
     cardBody.appendChild(cardText);
-    // cardBody.appendChild(buttonContainer);
 
     card.appendChild(cardBody);
 
     // Append the card to the cardContainer div
-    let cardContainer = document.getElementById('cardContainer');
+    let cardContainer = document.getElementById(CARD_CONTAINER_ID);
     cardContainer.appendChild(card);
 }
 
 function displayCards() {
-    let updatedText = document.getElementById("updatedText")
+    let updatedText = document.getElementById(UPDATED_TEXT_ID);
     let currentTime = new Date();
-    updatedText.textContent = "updated at: " + currentTime.toLocaleTimeString();
-    let arrayToIterate = JSON.parse(localStorage.getItem("testArray"));
+    updatedText.textContent = UPDATED_AT_TEXT + currentTime.toLocaleTimeString();
+    
+    let arrayToIterate = JSON.parse(localStorage.getItem(STORAGE_KEY));
 
-    let cardContainer = document.getElementById('cardContainer');
+    let cardContainer = document.getElementById(CARD_CONTAINER_ID);
     cardContainer.innerHTML = '';
 
     arrayToIterate.forEach(element => {
-        createCard(element.key, element.userText)
+        createCard(element.key, element.userText);
     });
 }
 
 function fetchEveryTwoSeconds() {
-    setInterval(displayCards, 2000)
+    setInterval(displayCards, INTERVAL_DELAY_MS);
 }
-
-
-
--- COMP4537/labs/1/js/writer.js
+++ COMP4537/labs/1/js/writer.js
@@ -1,11 +1,26 @@
+const STORAGE_KEY = "testArray";
+const EDIT_TEXT_ID = "editText";
+const USER_INPUT_EDIT_TEXT_ID = "userInputEditText";
+const CARD_CONTAINER_ID = "cardContainer";
+const EDIT_CONTAINER_ID = "editContainer";
+const STORE_UPDATE_TEXT_ID = "storeUpdateText";
+
+const EDIT_NOTE_TEXT = "Edit your notes here";
+const ENTER_NOTE_TEXT = "Enter your notes here";
+const SUBMIT_TEXT = "Submit";
+const EDIT_BUTTON_TEXT = "Edit";
+const DELETE_BUTTON_TEXT = "Delete";
+const STORED_AT_TEXT = "stored at: ";
+const UPDATED_AT_TEXT = "updated at: ";
+
 function initializeArrayIntoLocalStorage() {
-    let updatedText = document.getElementById("storeUpdateText")
+    let updatedText = document.getElementById(STORE_UPDATE_TEXT_ID)
     let currentTime = new Date();
     let userArray = []
 
-    updatedText.innerText = "stored at: " + currentTime.toLocaleTimeString();
-    if (localStorage.getItem("testArray") == null) {
-        localStorage.setItem("testArray", JSON.stringify(userArray));
+    updatedText.innerText = STORED_AT_TEXT + currentTime.toLocaleTimeString();
+    if (localStorage.getItem(STORAGE_KEY) == null) {
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(userArray));
     }
 
     displayCards()
@@ -16,7 +31,6 @@ function editCard(e) {
 }
 
 function displayEditFields(key) {
-
     // Create a form element
     let form = document.createElement("form");
     form.action = "/url"; // Set the action attribute
@@ -24,18 +38,18 @@ function displayEditFields(key) {
 
     // Create a paragraph element with an ID
     let paragraph = document.createElement("p");
-    paragraph.id = "editText";
-    paragraph.textContent = "Edit your notes here";
+    paragraph.id = EDIT_TEXT_ID;
+    paragraph.textContent = EDIT_NOTE_TEXT;
 
     // Create an input element
     let input = document.createElement("input");
     input.type = "text";
-    input.id = "userInputEditText";
+    input.id = USER_INPUT_EDIT_TEXT_ID;
 
     // Create a button element with an onclick event
     let button = document.createElement("button");
     button.type = "button";
-    button.textContent = "Submit";
+    button.textContent = SUBMIT_TEXT;
     button.id = key;
     button.onclick = getEditTextFromUser;
 
@@ -45,27 +59,27 @@ function displayEditFields(key) {
     form.appendChild(button);
 
     // Append the form to the container
-    let formContainer = document.getElementById("editContainer");
+    let formContainer = document.getElementById(EDIT_CONTAINER_ID);
     formContainer.appendChild(form);
 }
 
 function getEditTextFromUser(e) {
-    let userInput = document.getElementById("userInputEditText").value;
+    let userInput = document.getElementById(USER_INPUT_EDIT_TEXT_ID).value;
 
     let keyToEdit = e.target.id;
-    let localStorageArray = JSON.parse(localStorage.getItem("testArray"))
+    let localStorageArray = JSON.parse(localStorage.getItem(STORAGE_KEY))
     let index = localStorageArray.findIndex(x => x.key.toString() == keyToEdit)
 
     localStorageArray[index].userText = userInput
-    localStorage.setItem("testArray", JSON.stringify(localStorageArray))
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(localStorageArray))
 
-    let cardContainer = document.getElementById('cardContainer');
+    let cardContainer = document.getElementById(CARD_CONTAINER_ID);
     cardContainer.innerHTML = '';
 
-    let clearEditContainer = document.getElementById("editContainer");
+    let clearEditContainer = document.getElementById(EDIT_CONTAINER_ID);
     clearEditContainer.innerHTML = "";
 
-    let arrayToIterate = JSON.parse(localStorage.getItem("testArray"));
+    let arrayToIterate = JSON.parse(localStorage.getItem(STORAGE_KEY));
     arrayToIterate.forEach(element => {
         createCard(element.key, element.userText)
     });
@@ -73,17 +87,15 @@ function getEditTextFromUser(e) {
 
 function deleteCard(e) {
     let keyToDelete = e.target.id;
-    let localStorageArray = JSON.parse(localStorage.getItem("testArray"))
+    let localStorageArray = JSON.parse(localStorage.getItem(STORAGE_KEY))
     let filteredArray = localStorageArray.filter(x => x.key.toString() != keyToDelete)
-    localStorage.setItem("testArray", JSON.stringify(filteredArray));
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(filteredArray));
 
     const divToRemove = document.getElementById(e.target.id)
     divToRemove.remove()
 }
 
 function createCard(key, value) {
-    // console.log({ key, value })
-
     let card = document.createElement('div');
     card.id = key;
     card.className = 'card';
@@ -109,7 +121,7 @@ function createCard(key, value) {
     editButton.onclick = editCard;
     editButton.href = '#';
     editButton.className = 'btn btn-primary';
-    editButton.textContent = 'Edit';
+    editButton.textContent = EDIT_BUTTON_TEXT;
 
     // Create Delete button
     let deleteButton = document.createElement('a');
@@ -117,7 +129,7 @@ function createCard(key, value) {
     deleteButton.onclick = deleteCard;
     deleteButton.href = '#';
     deleteButton.className = 'btn btn-primary';
-    deleteButton.textContent = 'Delete';
+    deleteButton.textContent = DELETE_BUTTON_TEXT;
 
     // Append elements to their respective parent elements
     buttonContainer.appendChild(editButton);
@@ -129,13 +141,13 @@ function createCard(key, value) {
     card.appendChild(cardBody);
 
     // Append the card to the cardContainer div
-    let cardContainer = document.getElementById('cardContainer');
+    let cardContainer = document.getElementById(CARD_CONTAINER_ID);
     cardContainer.appendChild(card);
 }
 
 function displayCards() {
-    let arrayToIterate = JSON.parse(localStorage.getItem("testArray"));
-    let cardContainer = document.getElementById('cardContainer');
+    let arrayToIterate = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    let cardContainer = document.getElementById(CARD_CONTAINER_ID);
     
     cardContainer.innerHTML = '';
     arrayToIterate.forEach(element => {
@@ -143,36 +155,34 @@ function displayCards() {
     });
 }
 
-function TestObject(key, userText) {
+function UserObject(key, userText) {
     this.key = key;
     this.userText = userText;
 }
 
 function getTextFromUser() {
     let userInput = document.getElementById("userInput").value;
-    let editTextP = document.getElementById("editText");
+    let editTextP = document.getElementById(EDIT_TEXT_ID);
     let uniqueKey = Date.now()
-    let testObject = new TestObject(uniqueKey, userInput);
-    let localStorageArray = JSON.parse(localStorage.getItem("testArray") || "[]")
+    let userObject = new UserObject(uniqueKey, userInput);
+    let localStorageArray = JSON.parse(localStorage.getItem(STORAGE_KEY) || "[]")
 
-    localStorageArray.push(testObject)
-    localStorage.setItem("testArray", JSON.stringify(localStorageArray))
+    localStorageArray.push(userObject)
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(localStorageArray))
 
-    if (editTextP.innerText == "Edit your notes here") {
-        editTextP.innerText = "Enter your notes here"
+    if (editTextP.innerText == EDIT_NOTE_TEXT) {
+        editTextP.innerText = ENTER_NOTE_TEXT;
     }
 
     document.getElementById("userInput").value = ""
 }
 
 function storeCards() {
-    let updatedText = document.getElementById("storeUpdateText")
+    let updatedText = document.getElementById(STORE_UPDATE_TEXT_ID)
     let currentTime = new Date();
-    updatedText.innerText = "updated at: " + currentTime.toLocaleTimeString();
-
+    updatedText.innerText = UPDATED_AT_TEXT + currentTime.toLocaleTimeString();
 }
 
 function storeEveryTwoSeconds() {
     setInterval(initializeArrayIntoLocalStorage, 2000)
 }
-
